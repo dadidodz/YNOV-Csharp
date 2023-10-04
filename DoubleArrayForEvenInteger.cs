243955bb6630a927611523db00d414568a46dab5
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-04 17:19:02 +0200

    DoubleArrayForEvenInteger

---
+++ DoubleArrayForEvenInteger.cs
@@ -0,0 +1,15 @@
+namespace CSharpDiscovery.Quest01
+{
+    public class DoubleArrayForEvenInteger_Exercice
+    {
+         public static int[] DoubleArrayForEvenInteger(int[] inputTab)
+        {
+            for (int i=0; i<inputTab.Length;i++){
+                if (inputTab[i]%2!=0){
+                    inputTab[i] *=2;
+                }
+            }
+            return inputTab;
+        }
+    }
+}
