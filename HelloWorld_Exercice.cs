c9e5326925f8dc3d81edcbd74d994c2a71a8d83a
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-04 15:58:24 +0200

    hello world

---
+++ HelloWorld_Exercice.cs
@@ -0,0 +1,11 @@
+namespace CSharpDiscovery.Quest01
+{
+    public class HelloWorld_Exercice
+    {
+        public static string HelloWorld()
+        {
+            string chaine ="Hello World !";
+            return chaine;
+        }
+    }
+}
