59b9065d0464e00aa431029027472357dd2b7271
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-04 16:32:07 +0200

    setpivalue

---
+++ SetPiValue.cs
@@ -0,0 +1,10 @@
+namespace CSharpDiscovery.Quest01
+{
+    public class SetPiValue_Exercice
+    {
+        public static float SetPiValue(float pi)
+        {
+            return pi;
+        }
+    }
+}
