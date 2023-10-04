b0a0e1418519845e65fa6ca7cf4cf5adbcc27cd4
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-04 16:19:48 +0200

    isodd

---
+++ IsOdd.cs
@@ -0,0 +1,15 @@
+namespace CSharpDiscovery.Quest01
+{
+    public class IsOdd_Exercice
+    {
+        public static bool IsOdd(int a)
+        {
+            bool resultat = true;
+            if (a%2==0 && a!=0) {
+                return resultat = false;
+            }else{
+                return resultat;
+            }
+        }
+    }
+}
