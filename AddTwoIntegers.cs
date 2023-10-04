f02ecc6d26e0d53be49d1f2b06f9fe010ff9bfca
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-04 16:07:42 +0200

    addtwoints

---
+++ AddTwoIntegers.cs
@@ -0,0 +1,11 @@
+namespace CSharpDiscovery.Quest01
+{
+    public class AddTwoIntegers_Exercice
+    {
+        public static int AddTwoIntegers(int a, int b)
+        {
+            int resultat = a + b;
+            return resultat;
+        }
+    }
+}
