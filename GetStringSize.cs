75c8c15edc23109b8f9b2a43101413e527cec34c
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-04 17:28:20 +0200

    GetStringSize

--- GetStringSize.cs
+++ GetStringSize.cs
@@ -4,6 +4,10 @@ namespace CSharpDiscovery.Quest01
     {
         public static int GetStringSize(string str)
         {
+            int resultat = str.Length;
+            if(resultat==null){
+                return 0;
+            }
             return str.Length;
         }
     }
