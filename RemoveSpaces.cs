a4782789be4cb8673ef272e90ccc0bb0e0d524ad
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-04 17:56:26 +0200

    RemoveSpaces

--- RemoveSpaces.cs
+++ RemoveSpaces.cs
@@ -4,15 +4,7 @@ namespace CSharpDiscovery.Quest01
     {
         public static string RemoveSpaces(string str)
         {
-            char[] arr;
-            arr = str.ToCharArray(0, str.Length);
-            string resultat ="";
-            for (int i=0; i<arr.Length; i++){
-                if(arr[i]!=' '){
-                    resultat += Char.ToString(arr[i]);
-                }
-            }
-            return resultat;
+            return str.Trim();
         }
     }
 }
