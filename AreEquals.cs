7227eabaa0e36a3c186337f5cba490ef7aa63298
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-06 15:36:55 +0200

    AreEquals

---
+++ AreEquals.cs
@@ -0,0 +1,15 @@
+namespace CSharpDiscovery.Quest02
+{
+    public class AreEquals_Exercice
+    {
+        public static bool AreEquals(int[] tab1, int[] tab2)
+        {
+              if (tab1 == tab2)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
