ce38dae497746a5886fbd71beb0fe3efa056a1cc
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-06 15:32:21 +0200

    FromStringToDateTime

--- FromStringToDateTime.cs
+++ FromStringToDateTime.cs
@@ -1,10 +1,23 @@
+using System.Globalization;
+
 namespace CSharpDiscovery.Quest02
 {
     public class FromStringToDateTime_Exercice
     {
         public static DateTime FromStringToDateTime(string dateStr)
         {
-            return DateTime.Parse(dateStr);
+            if (string.IsNullOrEmpty(dateStr))
+                throw new ArgumentException("Input string cannot be null or empty.");
+
+            try
+            {
+                DateTime result = DateTime.ParseExact(dateStr, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+                return result;
+            }
+            catch (FormatException)
+            {
+                throw new FormatException($"Input string '{dateStr}' is not in the correct format.");
+            }
         }
     }
 }
