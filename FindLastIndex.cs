03c4b6d8a09f43834a501a644e67945b1e7907e5
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-07 17:25:35 +0200

    FindLastIndex

--- FindLastIndex.cs
+++ FindLastIndex.cs
@@ -4,19 +4,20 @@ namespace CSharpDiscovery.Quest02
     {
         public static int? FindLastIndex(int[] tab, int a)
         {
-            if (tab == null || tab.Length == 0  ){
-                throw new System.ArgumentNullException(nameof(tab), "is null");
-            } else  if (Array.LastIndexOf(tab, a)<0){
-                return null;
-            }
-            else
-            {
-                return Array.LastIndexOf(tab, a);
+            try{
+                if (tab == null || tab.Length == 0  ){
+                    throw new System.ArgumentNullException(nameof(tab), "is null");
+                } else  if (Array.LastIndexOf(tab, a)<0){
+                    return null;
+                }
+                else
+                {
+                    return Array.LastIndexOf(tab, a);
             } 
-
-           
-    
-                    
-        }
+ 
+            }catch(ArgumentNullException){
+                throw new ArgumentNullException($"Input string '{tab}' is not in the correct format.");
+            }
+        }      
     }
 }
