f8b2bff8da197b36266fab254fbb8f72e2979824
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-09 11:26:59 +0200

    PointOfInterest

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -1,17 +1,23 @@
 namespace CSharpDiscovery.Quest03;
 
-public class PointOfInterest{
+    public class PointOfInterest{
 
-        public double Latitude;
-
-        public double Longitude;
-        public string Name;
+        private double latitude;
+        public double Latitude{
+            get { return latitude; }
+            set { latitude = 0.0d; }
+        }
+        private double longitude;
+        public double Longitude{
+            get { return longitude; }
+            set { longitude = 0.0d; }
+        }
+        private string name;
+        public string Name   // property
+        {
+            get { return name; }
+            set { name = "f "; }
+        }
 
         public static string GoogleMapsUrlTemplate = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
-
-        public PointOfInterest(){
-            Latitude = 0;
-            Longitude = 0;
-            Name = "";
-        }
     }
