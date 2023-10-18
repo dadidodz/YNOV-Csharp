d7f234b838597be394ba9563e8668630d8578153
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-18 14:17:19 +0200

    Campus

---
+++ Campus.cs
@@ -0,0 +1,11 @@
+namespace CSharpDiscovery.Quest03;
+
+public class Campus : PointOfInterest {
+    public int Capacity;
+
+    public override string ToString()
+    {
+        return $"{Name} is a campus with a capacity of {Capacity} students";
+    }
+
+}
